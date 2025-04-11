eb4319b3fd537e3f3c5889174a3389621e6673bf
AgcioVF <alberto.viserasf@gmail.com>
2025-04-11 10:58:08 +0200

    Modificación de la estructura

--- src/main/CarrilBiciManager.java
+++ src/main/CarrilBiciManager.java
@@ -14,7 +14,6 @@ import java.util.NoSuchElementException;
  * Esta clase tiene como funcionalidad el control y gestión del carril bici.
  *  Cuenta con dos mapas constantes que almacenan como valor la longitud de los tramos
  *  y su estado respectivamente
- *
  */
 public class CarrilBiciManager {
 
@@ -25,7 +24,6 @@ public class CarrilBiciManager {
      * Objeto de la clase
      *
      * Al crearlo declara los mapas para su posterior uso
-     *
      */
     public CarrilBiciManager() {
         this.tramos = new HashMap<>();
@@ -33,14 +31,11 @@ public class CarrilBiciManager {
     }
 
     /**
-     *
      * En base a los parámetros introducidos crea un par Clave-Valor para los mapas tramos y estadoTramos
      *
      * @param longitud double // Km del tramo
      * @param nombre String // nombre del tramo
-     *
      * @throws IllegalArgumentException Sucede cuando el nombre se encuentra vacio ("") o cuando la longitud es menor que 0.
-     *
      */
     public void anadirTramo(String nombre, double longitud) {
         if (nombre == null || nombre.isBlank()) {
@@ -54,14 +49,11 @@ public class CarrilBiciManager {
     }
 
     /**
-     *
      * Busca a que par clave-valor hace referencia el primer String y lo sustituye por el segundo
      *
      * @param nombre String // anterior nombre del tramo
      * @param nuevoEstado String // nuevo nombre del tramo
-     *
      * @throws NoSuchElementException Cuando no se encuentra el tramo
-     *
      */
     public void actualizarEstado(String nombre, String nuevoEstado) {
         if (!tramos.containsKey(nombre)) {
@@ -77,21 +69,18 @@ public class CarrilBiciManager {
      * @param estado String // valor
      *
      *@deprecated Intención de sustituirlo por actualizarEstado
-     *
      */
     public void cambiarEstado(String nombre, String estado) {
         actualizarEstado(nombre, estado);
     }
 
     /**
-     *
      * Permite comprobar la existencia de un tramo
      *
      * @param nombre String // clave a buscar
      * @throws NoSuchElementException Cuando no se encuentra el tramo
      *
      * @return String
-     *
      */
     public String consultarEstado(String nombre) {
         if (!estadoTramos.containsKey(nombre)) {
@@ -101,33 +90,26 @@ public class CarrilBiciManager {
     }
 
     /**
-     *
      * Suma todos los valores del mapa tramos y devuelve el resultado
      *
      * @return double
-     *
      */
     public double longitudTotal() {
         return tramos.values().stream().mapToDouble(Double::doubleValue).sum();
     }
 
     /**
-     *
      * @return Mapa inalterable de los tramos
-     *
      */
     public Map<String, Double> obtenerTramos() {
         return Collections.unmodifiableMap(tramos);
     }
 
     /**
-     *
      * Utiliza un stream de Java8 para generar proceduralmento por consola
      * toda la informacion contenida en ambos mapas
      *
      * @return Cadena de caracteres en formato to.String()
-     *
-     *
      */
     public String generarInforme() {
         StringBuilder sb = new StringBuilder("INFORME DE CARRILES BICI - Bahía de Cádiz\n");
